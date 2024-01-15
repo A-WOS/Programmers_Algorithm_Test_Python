4c7a20dacd87238ac1891559ddade588c6e7c034
AWOS <uws0331@gmail.com>
2024-01-15 23:33:58 +0900

    feat: 코테 입문 코드 풀기 - 주사위 게임 2

---
+++ SkillCheckTest/Level0/240115/주사위_게임_2.py
@@ -0,0 +1,13 @@
+def solution(a, b, c):
+    if a != b and b != c and c != a:
+        answer = a + b + c
+    elif a == b == c:
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+    else:
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+    return answer
+
+
+print(solution(2, 6, 1))  # 9
+print(solution(5, 3, 3))  # 473
+print(solution(4, 4, 4))  # 110592
